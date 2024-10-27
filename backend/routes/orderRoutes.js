effb9b68884604de089be994bb90dc30ad2526ba
Shukra Tamang <shukratamang.npl@gmail.com>
2024-10-27 12:33:49 +1100

    create order routes and order controller

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+import {
+  addOrderItems,
+  getMyOrders,
+  getOrderById,
+  updateOrderToPaid,
+  updateOrderToDelivered,
+  getOrders,
+} from "../controllers/orderController.js";
+import { userAuth, adminAuth } from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/", userAuth, addOrderItems);
+router.get("/", userAuth, adminAuth, getOrders);
+
+router.get("/myorder", userAuth, getMyOrders);
+router.get("/:id", userAuth, adminAuth, getOrderById);
+router.put("/:id/pay", userAuth, updateOrderToPaid);
+router.put("/:id/deliver", userAuth, adminAuth, updateOrderToDelivered);
+
+export default router;
