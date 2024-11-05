fa5115c9759fdbae08efc7d20cb6d5e4b7d35c00
Shukra Tamang <shukratamang.npl@gmail.com>
2024-11-05 20:33:00 +1100

    redesign product card

--- client/src/components/Product.jsx
+++ client/src/components/Product.jsx
@@ -5,23 +5,48 @@ import Rating from "./Rating";
 
 const Product = ({ product }) => {
   return (
-    <Card className="my-3 p-3 rounded">
+    <Card className="my-3 p-3 rounded h-100">
       <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant="top" />
+        <div
+          style={{
+            height: "200px",
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "#f8f8f8", // Optional: to give a uniform background
+          }}
+        >
+          <Card.Img
+            src={product.image}
+            variant="top"
+            style={{
+              maxHeight: "100%",
+              maxWidth: "100%",
+              objectFit: "contain",
+            }}
+          />
+        </div>
       </Link>
-      <Card.Body>
+      <Card.Body className="d-flex flex-column">
         <Link to={`/product/${product._id}`}>
-          <Card.Title as="div" className="product-title">
+          <Card.Title
+            as="div"
+            className="product-title text-truncate"
+            style={{ height: "40px", overflow: "hidden" }}
+          >
             <strong>{product.name}</strong>
           </Card.Title>
         </Link>
-        <Card.Text as="div">
+        <Card.Text as="div" className="my-2">
           <Rating
             value={product.rating}
             text={`${product.numReviews} Reviews`}
           />
         </Card.Text>
-        <Card.Text as="h3">${product.price}</Card.Text>
+        <Card.Text as="h3" className="mt-auto">
+          ${product.price}
+        </Card.Text>
       </Card.Body>
     </Card>
   );
