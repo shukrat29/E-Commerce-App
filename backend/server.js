43d6dca285cdb396af086d94c7bd99491ebcb859
Shukra Tamang <shukratamang.npl@gmail.com>
2024-11-05 14:56:21 +1100

    final clean up

--- backend/server.js
+++ backend/server.js
@@ -33,6 +33,20 @@ app.get("/api/config/paypal", (req, res) =>
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  // any route that is not api will be redirected to index.html
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+
 app.use(notFound);
 app.use(errorHandler);
 
